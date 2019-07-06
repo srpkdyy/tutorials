36a32c865b4df4eaf896602532156230bd3fbc26
srpkdyy <leader931334@outlook.jp>
2019-07-06 23:32:54 +0900

    Add import lines

--- gan/dcgan/main.py
+++ gan/dcgan/main.py
@@ -8,3 +8,14 @@ import torch.backends.cudnn as cudnn
 import torch.optim as optim
 import torch.utils.data
 import torchvision.datasets
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+manualSeed = 999
+#manualSeed = random.randint(1, 10000) # use if you want new results
+print("Random Seed:", manualSeed)
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
