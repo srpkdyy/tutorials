920c20f5035f5dc23cc6acf173a332ffca9fc94e
srpkdyy <leader931334@outlook.jp>
2019-07-08 01:00:50 +0900

    Implement optimizers both G and D

--- gan/dcgan/main.py
+++ gan/dcgan/main.py
@@ -81,8 +81,22 @@ def main():
     netG.apply(weights_init)
     netD.apply(weights_init)
 
+    print(device)
     print(netG)
     print(netD)
+
+    criterion = nn.BCELoss()
+
+    # Create batch of latent vectors that we will use to visualize
+    #  the progression of the generator
+    fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+
+    # Establish convention for real and fake labels during training
+    real_label = 1
+    fake_label = 0
+
+    optimizersD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+    optimizersG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
 
 
 def weights_init(m):
